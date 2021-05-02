07b24141117559b6c3e164b2af869f5bffe6bf11
Angel Mendez <angelmendezz751@gmail.com>
2021-05-01 22:08:58 -0500

    feat: Mostramos mensaje de carrito vacío

--- components/MainCart/index.jsx
+++ components/MainCart/index.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useState } from "react";
 import getVariants from "../../src/lib/getVariants";
 import CartInfo from "../Cart/CartInfo";
@@ -16,9 +17,29 @@ const MainCart = ({ data }) => {
     let newLineItems = lineItems.filter((el) => el.id !== data.id);
     setLineItems(newLineItems);
     console.log(subtotal);
-    setSubtotalAttributes(subtotal.data.attributes);
+    setSubtotalAttributes(subtotal.data.attributes || {});
   };
 
+  if (lineItems.length <= 0 ) {
+    return (
+      <div className="h-screen w-11/12 flex-wrap mx-auto flex items-center justify-center">
+        <div className="w-full relative flex-wrap mx-auto flex items-center justify-center ">
+          <div className="w-full text-center">
+            <h1 className="text-4xl w-full font-medium">
+              Tu carrito está vacío
+            </h1>
+          </div>
+          <div className="flex mt-12">
+            <Link href={`/products`}>
+              <a className="bg-black hover:bg-gray-800 transition duration-200 text-white rounded py-3 px-8 focus:outline-none">
+                Explorar productos
+              </a>
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="w-full">
       <div className="w-full py-20">
