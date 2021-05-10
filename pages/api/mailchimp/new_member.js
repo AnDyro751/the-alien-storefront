4cc7366ef261a68b85d5dd3ed9a1544ba5909555
Angel Mendez <angelmendezz751@gmail.com>
2021-05-10 18:37:24 -0500

    feat: Add new_member.js API end point for mailchimp subscribers

---
+++ pages/api/mailchimp/new_member.js
@@ -0,0 +1,29 @@
+import client from '@mailchimp/mailchimp_marketing'
+import isEmpty from 'lodash/isEmpty';
+
+client.setConfig({
+    apiKey: "75af2ebd2f919ea0e436e8f218a90968-us4",
+    server: "us4",
+});
+
+export default async (req, res) => {
+    if (req.method === "POST") {
+        if (isEmpty(req.body)) {
+            return res.status(401).json({success: false})
+        } else {
+            try {
+                const data = await client.lists.addListMember("94d9b6e312", {
+                    email_address: req.body.email,
+                    status: "pending",
+                })
+                return res.status(200).json({success: true, data});
+            } catch (e) {
+                return res.status(401).json({success: false, error: e?.response?.body?.title})
+            }
+        }
+    } else {
+        return res.status(404).json({success: false, error: "Invalid route"})
+    }
+    // console.log(response);
+
+}
